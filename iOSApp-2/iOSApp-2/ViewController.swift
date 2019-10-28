dba9b1dc9e1bc471909032dbb2b96507cdf49244
veerababum <veerababum@users.noreply.github.com>
2019-10-27 23:11:22 -0700

    Connect the UI to Code

--- iOSApp-2/iOSApp-2/ViewController.swift
+++ iOSApp-2/iOSApp-2/ViewController.swift
@@ -8,23 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    
     @IBOutlet weak var mealName: UILabel!
     @IBOutlet weak var EntertheNameTextField: UITextField!
-
-
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // Handle the text field’s user input through delegate callbacks.
+        EntertheNameTextField.delegate = self
     }
+    // MARK: Actions
     @IBAction func SetDefaultMealName(_ sender: Any) {
         
-        mealName.text = "Default Text"
-        
+        //mealName.text = "Default Text"
         
         
     }
-
+    
+    //MARK: Text Fileld delegate methods
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+        mealName.text = textField.text
+        
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        //Hide the keyboard
+        textField.resignFirstResponder()
+        return true
+    }
 }
 
